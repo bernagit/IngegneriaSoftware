1c5dc050f9f58cfd2d43f74693da2c2adf22a98b
Alessandro Bernocchi <60480768+bernagit@users.noreply.github.com>
2022-10-14 11:19:26 +0200

    Test complete for InserisciGerarchia

--- src/test/java/controller/InserisciGerarchiaTest.java
+++ src/test/java/controller/InserisciGerarchiaTest.java
@@ -24,10 +24,17 @@ class InserisciGerarchiaTest {
         }
         System.setIn(systemIn);
         gerarchia = inserisci.inserisciGerarchia();
+        //test
         assertEquals(gerarchia.getNomeRadice(), "scarpe");
+        assertEquals(gerarchia.getRadice().getFigli().size(), 3);
         assertEquals(gerarchia.getRadice().getFigli().get(2).getNome(), "eleganti");
+        assertEquals(gerarchia.getRadice().getFigli().get(2).getFigli().size(), 2);
         assertEquals(gerarchia.getRadice().getFigli().get(2).getFigli().get(0).getNome(), "mocassini");
         assertTrue(gerarchia.getRadice().getFigli().get(0).isFoglia());
         assertTrue(gerarchia.getRadice().getFigli().get(2).getFigli().get(0).isFoglia());
+        assertEquals(gerarchia.getRadice().getFigli().get(2).getFigli().get(0).getPadre(), "eleganti");
+        assertEquals(gerarchia.getRadice().getFigli().get(2).getFigli().get(0).getCampi().get(0).getNome(), "Stato di conservazione");
+        assertEquals(gerarchia.getRadice().getFigli().get(2).getFigli().get(1).getCampi().get(4).getNome(), "altezza tacco");
+        assertTrue(gerarchia.getRadice().getFigli().get(2).getFigli().get(1).getCampi().get(4).isRequired());
     }
 }
