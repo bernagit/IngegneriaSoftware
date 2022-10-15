c0a9446180900078f608e71bd8a668ca991ded76
Alessandro Bernocchi <60480768+bernagit@users.noreply.github.com>
2022-10-15 11:16:54 +0200

    Test complete for LoginFruit

--- src/test/java/controller/LoginFruitTest.java
+++ src/test/java/controller/LoginFruitTest.java
@@ -3,10 +3,7 @@ package controller;
 import model.user.Fruitore;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,20 +11,26 @@ class LoginFruitTest {
     LoginFruit login = new LoginFruit();
     Fruitore fruitore;
     InputStream systemIn;
+
+    ByteArrayOutputStream systemOut = new ByteArrayOutputStream();
     @Test
     void dofirstLogin() {
-        File file = new File("./src/test/cases/FirstLoginFruit");
+        File file = new File("./src/test/cases/LoginFruit");
         try {
             systemIn = new FileInputStream(file);
         } catch (FileNotFoundException e) {
             System.out.println("File non trovato");
         }
         System.setIn(systemIn);
+        System.setOut(new PrintStream(systemOut));
+
         fruitore = (Fruitore) login.doLogin();
 
+        assertEquals("Primo Login? (S/N): Inserisci username: Inserisci password: ", systemOut.toString());
         assertNotEquals(fruitore, null);
         assertEquals(fruitore.getUsername(), "niccolo");
         assertFalse(fruitore.getUserType());
+
     }
 
 }
