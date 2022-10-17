ab5b0619c408a7ecbbe660b03e223d8e1588d951
michelinux <m.ce@studenti.unibs.it>
2022-10-17 20:39:55 +0200

    RitiraOffertaTest.java with junit Berna dimmi se va bene, ma direi che un'offerta così per provarla bisogna crearla al momento

--- src/test/java/controller/RitiraOffertaTest.java
+++ src/test/java/controller/RitiraOffertaTest.java
@@ -4,18 +4,34 @@ import model.gerarchia.Categoria;
 import model.offerta.Offerta;
 import model.offerta.StatoOfferta;
 import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RitiraOffertaTest {
+    VisualizzaOfferteProprietario visualizza;
     Offerta offerta;
-
+    InputStream systemIn;
     @Test
     void ritiraOfferta() {
         offerta = new Offerta("Offerta aperta test", new Categoria("Prova","Test",new ArrayList<>(),""), "Michele");
-        offerta.setStatoCorrente(StatoOfferta.APERTA);
+        offerta.setStatoCorrente(StatoOfferta.APERTA); //di default alla creazione di un'offerta da parte del fruitore
 
-        offerta.setStatoCorrente(StatoOfferta.RITIRATA);
+        visualizza = new VisualizzaOfferteProprietario();
+
+        File file = new File("./src/test/cases/RitiraOfferta");
+        try {
+            systemIn = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File non trovato");
+        }
+
+        System.setIn(systemIn);
+        visualizza.modificaOfferta(offerta);
+
         assertEquals(offerta.getStatoCorrente(), StatoOfferta.RITIRATA);
     }
 }
