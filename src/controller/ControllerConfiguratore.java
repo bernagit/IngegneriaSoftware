4e236e46f19daa678604961ef0b568738b246c14
Alessandro Bernocchi <60480768+bernagit@users.noreply.github.com>
2022-11-11 18:13:04 +0100

    Information Expert su controller??

--- src/controller/ControllerConfiguratore.java
+++ src/controller/ControllerConfiguratore.java
@@ -1,38 +1,66 @@
 package controller;
 
-import controller.handlers.LoginConf;
-import controller.handlers.Logout;
+import controller.handlers.*;
 import model.user.Configuratore;
-import view.CliView;
 import view.View;
+import java.util.ArrayList;
 
 public class ControllerConfiguratore {
-    public View view;
-
+    final private View view;
+    final private ArrayList<Option> options = new ArrayList<>();
     public ControllerConfiguratore(View view) {
         this.view = view;
     }
     public void run(){
-        OptionList option = new OptionList();
         String titolo;
         Configuratore configuratore = null;
+        boolean exit = false;
         do {
             view.createMenu("");
-            view.setVociMenu(option.getConfOptionList(configuratore));
+            this.setOption(configuratore);
+            view.setVociMenu(this.getVoci());
             if (configuratore != null) titolo = "Utente "+configuratore.getUsername()+ " loggato";
             else titolo = "Programma Configuratore";
             view.setTitoloMenu(titolo);
             int scelta = view.scegliVoceMenu();
             try {
-                Handler handler = option.getOption(scelta).getAction();
+                Handler handler = options.get(scelta).getAction();
                 if (handler instanceof LoginConf || handler instanceof Logout)
                     configuratore = (Configuratore) handler.execute(configuratore, view);
                 else
                     handler.execute(configuratore, view);
             } catch (ExitException e) {
                 view.print(e.getMessage());
-                System.exit(1);
+                exit = true;
             }
-        } while (true);
+        } while (!exit);
     }
+
+    private ArrayList<String> getVoci() {
+        ArrayList<String> voci = new ArrayList<>();
+        for (Option opt: options){
+            voci.add(opt.getLabel());
+        }
+        return voci;
+    }
+
+    private void setOption(Configuratore conf){
+        options.clear();
+        options.add(new Option("Esci", new Exit()));
+        if (conf == null){
+            options.add(new Option("Login", new LoginConf()));
+            options.add(new Option("Visualizza Gerarchie", new VisualizzaGerarchie()));
+        }
+        else {
+            options.add(new Option("Logout", new Logout()));
+            options.add(new Option("Visualizza Gerarchie", new VisualizzaGerarchie()));
+            options.add(new Option("Inserisci Gerarchia", new InserisciGerarchia()));
+            options.add(new Option("Aggiungi Scambio", new InserisciScambio()));
+            options.add(new Option("Visualizza Offerte Aperte", new VisualizzaOfferte()));
+            options.add(new Option("Visualizza Offerte Chiuse o In Scambio", new VisualizzaOfferteConfig()));
+            options.add(new Option("Importa File Configurazione", new ImportaFileConf()));
+        }
+    }
+
+
 }
--- src/controller/ControllerFruitore.java
+++ src/controller/ControllerFruitore.java
@@ -1,41 +1,68 @@
 package controller;
 
-import controller.handlers.LoginFruit;
-import controller.handlers.Logout;
+import controller.handlers.*;
 import db.JsonManager;
 import model.user.Fruitore;
 import view.View;
 
-public class ControllerFruitore {
-    private View view;
+import java.util.ArrayList;
 
+public class ControllerFruitore {
+    final private View view;
+    final private ArrayList<Option> options = new ArrayList<>();
     public ControllerFruitore(View view){
         this.view = view;
     }
 
     public void run(){
-        OptionList option = new OptionList();
         Fruitore fruitore = null;
         String titolo;
+        boolean exit = false;
         do {
             // eliminazione dei baratti Scaduti
             JsonManager.eliminaBarattiScaduti();
             view.createMenu("");
-            view.setVociMenu(option.getFruitOptionList(fruitore));
+            this.setOption(fruitore);
+            view.setVociMenu(this.getVoci());
             if (fruitore != null) titolo = "Utente "+fruitore.getUsername()+" loggato";
             else titolo = "Programma fruitore";
             view.setTitoloMenu(titolo);
             int scelta = view.scegliVoceMenu();
             try {
-                Handler handler = option.getOption(scelta).getAction();
+                Handler handler = options.get(scelta).getAction();
                 if (handler instanceof LoginFruit || handler instanceof Logout)
                     fruitore = (Fruitore) handler.execute(fruitore, view);
                 else
                     handler.execute(fruitore, view);
             } catch (ExitException e) {
                 view.print(e.getMessage());
-                System.exit(1);
+                exit = true;
             }
-        } while (true);
+        } while (!exit);
+    }
+    private ArrayList<String> getVoci() {
+        ArrayList<String> voci = new ArrayList<>();
+        for (Option opt: options){
+            voci.add(opt.getLabel());
+        }
+        return voci;
+    }
+    private void setOption(Fruitore fruitore) {
+        options.clear();
+        options.add(new Option("Esci", new Exit()));
+        if (fruitore == null){
+            options.add(new Option("Login", new LoginFruit()));
+        }
+        else {
+            options.add(new Option("Logout", new Logout()));
+            options.add(new Option("Visualizza Scambi", new VisualizzaScambi()));
+            options.add(new Option("Pubblica Offerta", new PubblicaOfferta()));
+            options.add(new Option("Visualizza Offerte Aperte", new VisualizzaOfferte()));
+            options.add(new Option("Visualizza o Modifica Offerte Inserite", new VisualizzaOfferteProprietario()));
+            options.add(new Option("Baratta Oggetto", new BarattaOfferta()));
+            options.add(new Option("Accetta Baratto", new AccettaBaratto()));
+            options.add(new Option("Visualizza e/o Accetta Appuntamenti", new ModificaAppuntamento()));
+        }
+
     }
 }
